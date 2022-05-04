ab2bd21a8e96faa49aa4ce6a2b4ad1807423aa23
rikiansyaharis <rikiansyah.aris29@gmail.com>
2022-05-04 08:30:09 +0700

    Create scanner

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:qr_scanner/first.dart';
+import 'package:qr_scanner/second.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 1,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.indigo[900],
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.qr_code_scanner,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
+          title: Text(
+            '  QR / Barcode Scanner',
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Second(),
+          ],
+        ),
+      ),
+    );
+  }
+}
